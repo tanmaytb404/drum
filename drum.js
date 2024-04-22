a256e2d0963069c056a266f26c1d97ea91fed541
tanmaytb404 <83444921+tanmaytb404@users.noreply.github.com>
2024-04-22 16:40:24 +0530

    Update drum.js

--- drum.js
+++ drum.js
@@ -36,3 +36,42 @@ document.body.addEventListener("keypress", (event) => {
     }
 
 })
+
+document.body.addEventListener("click", (event) => {
+    if (event.key == "w") {
+        const audioW = document.createElement("audio");
+        audioW.src = "https://files.codingninjas.in/tom-1-28537.mp3?_ga=2.101095043.301608824.1713425657-826516099.1706512650";
+        audioW.play();
+    }
+    if (event.key == "a") {
+        const audioA = document.createElement("audio");
+        audioA.src = "https://files.codingninjas.in/tom-2-28541.mp3?_ga=2.130407181.301608824.1713425657-826516099.1706512650";
+        audioA.play();
+    }
+    if (event.key == "s") {
+        const audioS = document.createElement("audio");
+        audioS.src = "https://files.codingninjas.in/tom-3-28542.mp3?_ga=2.121680393.301608824.1713425657-826516099.1706512650";
+        audioS.play();
+    }
+    if (event.key == "d") {
+        const audioD = document.createElement("audio");
+        audioD.src = "https://files.codingninjas.in/tom-4-28543.mp3?_ga=2.121680393.301608824.1713425657-826516099.1706512650";
+        audioD.play();
+    }
+    if (event.key == "j") {
+        const audioK = document.createElement("audio");
+        audioK.src = "https://files.codingninjas.in/crash-28546.mp3?_ga=2.91029400.301608824.1713425657-826516099.1706512650";
+        audioK.play();
+    }
+    if (event.key == "k") {
+        const audioK = document.createElement("audio");
+        audioK.src = "https://files.codingninjas.in/crash-28546.mp3?_ga=2.91029400.301608824.1713425657-826516099.1706512650";
+        audioK.play();
+    }
+    if (event.key == "l") {
+        const audioL = document.createElement("audio");
+        audioL.src = "https://files.codingninjas.in/kick-bass-28547.mp3?_ga=2.91029400.301608824.1713425657-826516099.1706512650";
+        audioL.play();
+    }
+
+})
